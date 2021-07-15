8e62b8346da5f793e1a5d73c655a14731a0d7402
vishal kumar <vkumar61@slb.com>
2021-07-15 13:25:00 +0530

    Organizing Component Files

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
-import Card from "./Card";
+import '../Expenses/Expenses.css';
+import Card from "../UI/Card";
 
 function Expenses(props) {
   return (
