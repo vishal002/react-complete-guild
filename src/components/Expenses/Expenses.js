0ba0d886c60e27b27f065edc9b7652e1c1d26833
vishal kumar <vkumar61@slb.com>
2021-07-16 12:21:29 +0530

    Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import '../Expenses/Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
-    <Card className="expenses">
+    <div>
+      <Card className="expenses">
+      <ExpensesFilter selected={filteredYear}  onChangeFilter = {filterChangeHandler} />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -26,6 +36,7 @@ const Expenses = (props) => {
         date={props.expenses[3].date}
       ></ExpenseItem>
     </Card>
+    </div>
   );
 }
 
