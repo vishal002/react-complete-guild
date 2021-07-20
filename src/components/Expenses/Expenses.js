4cd494728d32dde0ead03f5e3a0a8b7903b77d38
vishal kumar <vkumar61@slb.com>
2021-07-20 16:11:38 +0530

    Adding Conditional Return Statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import "../Expenses/Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -14,19 +14,6 @@ const Expenses = (props) => {
   const filterExpenses = props.expenses
   .filter( expense => expense.date.getFullYear().toString() === filteredYear)
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if(filterExpenses.length > 0) {
-    expensesContent = filterExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -34,7 +21,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filterExpenses} />
       </Card>
     </div>
   );
